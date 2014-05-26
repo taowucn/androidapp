0427cef019b0cf1e8db2348ec58f667956837195
taowu <wtomybo@gmail.com>
2014-05-27 00:01:57 +0800

    add buildDrive, Notice:touch button will cause error

--- agent/src/com/robot/agent/MainActivity.java
+++ agent/src/com/robot/agent/MainActivity.java
@@ -6,12 +6,19 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.app.TabActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.View.OnTouchListener;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TabHost;
@@ -27,7 +34,10 @@ public class MainActivity extends TabActivity {
 	private String deviceAddress;
 	private LinearLayout layout;
 	private ScrollView scrollView;
-
+	private Robot robot;
+	
+	private Handler handler = new Handler();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -39,7 +49,7 @@ public class MainActivity extends TabActivity {
 		scrollView = (ScrollView) findViewById(R.id.tab1);
 
 		buildTabView();
-
+		buildDrive();
 
 //		if (savedInstanceState == null) {
 //			getSupportFragmentManager().beginTransaction()
@@ -69,6 +79,21 @@ public class MainActivity extends TabActivity {
 		tabHost.setCurrentTab(0);
 		tabHost.setOnTabChangedListener(tabChangeListener);
 	}
+	
+	private void buildDrive() {
+		ImageButton upBtn = (ImageButton) this.findViewById(R.id.up);
+		upBtn.setOnTouchListener(onTouchListener);
+		ImageButton downBtn = (ImageButton) this.findViewById(R.id.down);
+		downBtn.setOnTouchListener(onTouchListener);
+		ImageButton leftBtn = (ImageButton) this.findViewById(R.id.left);
+		leftBtn.setOnTouchListener(onTouchListener);
+		leftBtn.setOnClickListener(driveClickListener);
+		ImageButton rightBtn = (ImageButton) this.findViewById(R.id.right);
+		rightBtn.setOnTouchListener(onTouchListener);
+		rightBtn.setOnClickListener(driveClickListener);
+		ImageButton stopBtn = (ImageButton) this.findViewById(R.id.stop);
+		stopBtn.setOnClickListener(driveClickListener);
+	}
 
 	/** change tab */
 	private OnTabChangeListener tabChangeListener = new OnTabChangeListener() {
@@ -76,7 +101,116 @@ public class MainActivity extends TabActivity {
 			Log.d(TAG, "" + tabId);
 		}
 	};
-	
+
+	private OnKeyListener driveKeyListener = new OnKeyListener() {
+		public boolean onKey(View view, int keyCode, KeyEvent event) {
+			switch (event.getAction()) {
+			
+			case KeyEvent.ACTION_DOWN: {
+				Log.d(TAG, "key.id=" + keyCode);
+				switch (keyCode) {
+				case R.id.left:
+				case R.id.right: {
+					robot.stop();
+				}
+					break;
+				}
+				//display.append("down");
+			}
+				break;
+			case KeyEvent.ACTION_UP: {
+				switch (keyCode) {
+				case R.id.up:
+				case R.id.down:
+					// robot.setSpeed((byte) 0x10, (byte) 0x10);
+					break;
+				case R.id.left:
+				case R.id.right: {
+					robot.stop();
+				}
+					break;
+				}
+				//display.append("up");
+			}
+				break;
+			}
+			return false;
+		}
+	};
+
+	private OnClickListener driveClickListener = new OnClickListener() {
+		public void onClick(View view) {
+			Log.d(TAG, "view.id=" + view.getId());
+			switch (view.getId()) {
+			case R.id.left:
+			case R.id.right:
+			case R.id.stop:{
+				robot.stop();
+			}
+				break;
+			}
+			display.append("stop\n");
+			handler.post(scrollToBottom);
+		}
+	};
+	private OnTouchListener onTouchListener = new OnTouchListener() {
+
+		public boolean onTouch(View view, MotionEvent event) {
+			Log.d(TAG, "event.action=" + view.getId());
+			switch (event.getAction()) {
+			case MotionEvent.ACTION_MOVE: {
+				dispatchMsg(view.getId());
+				break;
+			}
+			default:
+				break;
+			}
+			return false;
+		}
+	};
+
+	private void dispatchMsg(int id) {
+		String msg = "no msg";
+		switch (id) {
+
+		case R.id.up: {
+			robot.changeSpeed((byte) 0x01, (byte) 0x01);
+			// msg = CommandUtil.driveMotorS(this, (byte) 0xff, (byte) 0xff);
+			break;
+		}
+		case R.id.down: {
+			robot.changeSpeed((byte) (-0x01), (byte) (-0x01));
+			// msg = CommandUtil.driveMotorS(this, (byte) 0x00, (byte) 0x00);
+			break;
+		}
+		case R.id.left: {
+			//robot.stop();
+			robot.changeSpeed((byte) (-0x01), (byte) (0x01));
+			// msg = CommandUtil.driveMotorS(this, (byte) 0x00, (byte) 0xff);
+			break;
+		}
+		case R.id.right: {
+			//robot.stop();
+			robot.changeSpeed((byte) (0x01), (byte) (-0x01));
+			// msg = CommandUtil.driveMotorS(this, (byte) 0xff, (byte) 0x00);
+			break;
+		}
+		case R.id.stop: {
+			robot.stop();
+			// msg = CommandUtil.driveMotorS(this, (byte) 0x80, (byte) 0x80);
+			break;
+		}
+		default:
+			break;
+
+		}
+		msg = "left:" + robot.getLeftSpeed() + " Right:"
+				+ robot.getRightSpeed();
+		display.append("Robot Speed:" + msg + "\n");
+		handler.post(scrollToBottom);
+	}
+
+
 	private Thread scrollToBottom = new Thread() {
 		@Override
 		public void run() {
